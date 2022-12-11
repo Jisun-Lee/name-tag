268fa1f468a80911e8460b49cd9625d759ab4155
Jisun_Lee <kimchilatkes@gmail.com>
2022-12-10 16:36:36 -0800

    added pronoun button, change name/pronoun on nametag on keyup

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // grab DOM elements
 
 // set event listeners
+
+const nameDisplay = document.getElementById('name-display');
+const nameInput = document.getElementById('name-input');
+const button = document.getElementById('update-button');
+
+button.addEventListener('click', () => {
+    nameDisplay.textContent = nameInput.value;
+});
+
+const pronounDisplay = document.getElementById('pronoun-display');
+const pronounInput = document.getElementById('pronoun-input');
+const pronounButton = document.getElementById('update-pronounButton');
+
+pronounButton.addEventListener('click', () => {
+    pronounDisplay.textContent = pronounInput.value;
+});
+
